73eb17177083fdf5abb431a0dd029921e62351b8
baraamir <baraamir@sheridancollege.ca>
2021-10-09 15:06:12 -0400

    first commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -6,6 +6,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
  * @author Ronak
  */
 public class CardTrick {
+    Card cards = new Card();
     public static void main(String[] args)
     {
        
